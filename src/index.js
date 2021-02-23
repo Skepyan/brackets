3f3dcd27720ca6de0ff66eb95eb660987e6a1301
unknown <you@example.com>
2021-02-23 11:16:08 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str === undefined || bracketsConfig === undefined){
+    return false
+  }
+  let cal = 0;
+  let value = bracketsConfig.flatMap(x => x.join(''));
+  
+  for (let i = 0; i < value.length; i++){
+    if (str.includes(value[i]) === false){
+      cal++;
+    }else{
+     str = str.replace(value[i], "");
+    }
+  }
+    if (cal === value.length){
+      return false;
+    }if (str.length === 0) {
+      return true;
+    }
+  
+return check(str, bracketsConfig)
 }
+
